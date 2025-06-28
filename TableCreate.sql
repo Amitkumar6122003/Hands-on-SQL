4503c32e05f1e901f2d1894a81f7e82d88274cf3
Amit Vikas Kadam <116537539+Amitkumar6122003@users.noreply.github.com>
2025-06-28 19:59:06 +0530

    Add files via upload

---
+++ TableCreate.sql
@@ -0,0 +1,13 @@
+select* from student;
+
+create table teacher(tid int,tname varchar(10),age int, tsubject varchar(20),salary varchar(30),gualification varchar(40),city varchar(50));
+insert into teacher values(1,"avi",35,"science",100000,"MBA","pune");
+insert into teacher values(1,"avi",35,"science",100000,"MBA","pune");
+insert into teacher values(1,"avi",35,"science",100000,"MBA","pune");
+insert into teacher values(1,"avi",35,"science",100000,"MBA","pune");
+insert into teacher values(1,"avi",35,"science",100000,"MBA","pune");
+insert into teacher values(1,"avi",35,"science",100000,"MBA","pune");
+insert into teacher values(1,"avi",35,"science",100000,"MBA","pune");
+insert into teacher values(1,"avi",35,"science",100000,"MBA","pune");
+
+select*from teacher;
